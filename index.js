3d1c1bba5294f9e6c9b7e539c4bb77f96125015f
Tobias Jansson <tobias.jansson@gmail.com>
2018-06-03 22:19:09 +0200

    Trying something new

--- index.js
+++ index.js
@@ -1,60 +1,77 @@
 const request = require('request');
 const url = require('url');
 
-var Accesory, Service, Characteristic;
+var Accesory, Service, Characteristic, DoorState;
 
 function mySwitch(log, config) {
     this.log = log;
     this.getUrl = url.parse(config['getUrl']);
     this.postUrl = url.parse(config['postUrl']);
+
+    this.currentDoorState = DoorState.CLOSED;
+    this.targetDoorState = DoorState.CLOSED;
   }
    
   mySwitch.prototype = {
    
     getSwitchOnCharacteristic: function (next) {
       const me = this;
+      me.log('uppdaterar');
       request({
           url: me.getUrl,
+          json: true,
           method: 'GET',
       }, 
       function (error, response, body) {
         if (error) {
-          me.log('STATUS: ' + response.statusCode);
-          me.log(error.message);
+          me.log('STATUS: ' + response);
+          me.log(error);
           return next(error);
         }
-        return next(null, body.currentState);
+        let state = 'open'?DoorState.OPEN:DoorState.CLOSED;
+        this.currentDoorState = state;
+        this.targetDoorState = state;      
+        return next(null, 
+          body.garagedoor === state);
       });
     },
      
     setSwitchOnCharacteristic: function (on, next) {
-      const me = this;
-      request({
-        url: me.postUrl,
-        body: {'targetState': on},
-        method: 'POST',
-        headers: {'Content-type': 'application/json'}
-      },
-      function (error, response) {
+        const me = this;
+        request({
+          url: me.getUrl,
+          json: true,
+          method: 'GET',
+      }, 
+      function (error, response, body) {
         if (error) {
-          me.log('STATUS: ' + response.statusCode);
-          me.log(error.message);
+          me.log('STATUS: ' + response);
+          me.log(error);
           return next(error);
         }
-        return next();
+        let state = 'open'?DoorState.OPEN:DoorState.CLOSED;
+        this.currentDoorState = state;
+        this.targetDoorState = state;      
+        return next(null, 
+          body.garagedoor === state);
       });
     },
-    
+
     getServices: function () {
         let informationService = new Service.AccessoryInformation();
         informationService
-            .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
-            .setCharacteristic(Characteristic.Model, "My switch model")
+            .setCharacteristic(Characteristic.Manufacturer, "Tobias")
+            .setCharacteristic(Characteristic.Model, "Min garagemodel")
             .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
 
-        let switchService = new Service.Switch("My switch");
+        let switchService = new Service.GarageDoorOpener("Garageporten", "Garageport");
         switchService
-            .getCharacteristic(Characteristic.On)
+            .getCharacteristic(Characteristic.CurrentDoorState)
+            .on('get', this.getSwitchOnCharacteristic.bind(this))
+            .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+        switchService
+            .getCharacteristic(Characteristic.TargetDoorState)
             .on('get', this.getSwitchOnCharacteristic.bind(this))
             .on('set', this.setSwitchOnCharacteristic.bind(this));
 
@@ -67,6 +84,7 @@ function mySwitch(log, config) {
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
+    DoorState = homebridge.hap.Characteristic.CurrentDoorState;
     homebridge.registerAccessory("homebridge-testplugin", "testplugin", mySwitch);
 };
 
